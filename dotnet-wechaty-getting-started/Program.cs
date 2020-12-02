c12fb0144fb896d56a0b885aa7bee02eb8576b4f
Darren <jesn2013@hotmail.com>
2020-12-02 10:28:27 +0800

    添加项目文件。

---
+++ dotnet-wechaty-getting-started/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_wechaty_getting_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
